65dac74d3266770c4906d47b7a12b5a60c5b818b
Petr Marek <Petr.Marek@tescosw.cz>
2022-02-25 19:01:29 +0100

    Project structure improved

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import React, {Component, MouseEventHandler} from "react"
+import classes from "./Button.module.scss"
+
+interface IButtonProps{
+    onClick: MouseEventHandler,
+    title: string
+}
+const Button = (props: IButtonProps) => {
+
+
+    return (
+        <div className={"button-wrapper"}>
+            <button className={classes.button} onClick={props.onClick}>{props.title}</button>
+        </div>
+
+    )
+}
+
+export default Button;
